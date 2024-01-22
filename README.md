2cae6139b56ca5a0c87d6a3abfd2464751260880
Jaime Lopez <jlopsan014@gmail.com>
2024-01-22 18:29:08 +0100

    Añadidos los cambios en el readme

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ El precio final debe incluir el 21% del impuesto general a las ventas, IVA.
 Diseñe un algoritmo que determine el precio a pagar por el alquiler de un vehículo y el totalincluido el impuesto.
 
 #2. Realiza un programa que pida al usuario dos enteros, estos enteros se pasan como parámetros a una función que muestra todos los números comprendidos entre ellos, inclusive.
+He cambiado el nombre de las variables por unos mas correctos, he indentado el codigo y he corregido errores ortograficos
 #3. Escribir una aplicación que solicite al usuario cuántos números desea introducir. A continuación, se introducirá por teclado esa cantidad de números enteros, y por último, los mostrará en el orden inverso al introducido.
+
 #4.Diseñar un programa que solicite al usuario una frase y una palabra. A continuaciónbuscará cuántas veces aparece la palabra en la frase.
+
 #5.Algoritmo que lea caracteres hasta que el carácter leído sea una Z y nos muestre elnúmero de caracteres leídos que son vocales.
--- Tarea_Desarrollo_Colaborativo/src/EjerciciosParaRefactorizar/Ejercicio2.java
+++ Tarea_Desarrollo_Colaborativo/src/EjerciciosParaRefactorizar/Ejercicio2.java
@@ -3,21 +3,29 @@ package EjerciciosParaRefactorizar;
 import java.util.Scanner;
 
 public class Ejercicio2 {
-	
-public static String numComprendidos(int n1, int n2) {
-String numeroscomprendidos = ""; for (int i = n1; i<= n2; i++) { numeroscomprendidos = numeroscomprendidos + " " + i;
-}return numeroscomprendidos;}
-public static void main(String[] args) {
-var sc = new Scanner(System.in);
-System.out.println("Introduce un número: ");
-int primernumero = sc.nextInt();
-System.out.println("Introduce un número mayor al anterior: ");
-int segundonumero = sc.nextInt();
-if(segundonumero < primernumero){System.out.println("Error. Introduce un número mayor al anterior: ");
-segundonumero = sc.nextInt();
-}System.out.println("Los números comprendidos entre " + primernumero + "y" + segundonumero + " son " + numComprendidos(primernumero,segundonumero));
-sc.close();
-	}}
 
+	public static void main(String[] args) {
+		var sc = new Scanner(System.in);
+		System.out.println("Introduce un numero: ");
+		int primerNumero = sc.nextInt();
+		System.out.println("Introduce un numero mayor al anterior: ");
+		int segundoNumero = sc.nextInt();
 
+		if (segundoNumero < primerNumero) {
+			System.out.println("Error. Introduce un nï¿½mero mayor al anterior: ");
+			segundoNumero = sc.nextInt();
+		}
 
+		System.out.println("Los numeros comprendidos entre " + primerNumero + " y " + segundoNumero + " son"
+				+ numComprendidos(primerNumero, segundoNumero));
+		sc.close();
+	}
+
+	public static String numComprendidos(int primerNumero, int segundoNumero) {
+		String numerosComprendidos = "";
+		for (int i = primerNumero; i <= segundoNumero; i++) {
+			numerosComprendidos = numerosComprendidos + " " + i;
+		}
+		return numerosComprendidos;
+	}
+}
